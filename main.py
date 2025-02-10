8ad4dc99ead602de3ca358d1117a1c613b7226be
bigdud24 <officialcoloriot@gmail.com>
2025-02-10 14:22:54 -0600

    functional prototype

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
-
-# determine screen dimensions and asteroid parameters
 from constants import *
+from circleshape import *
+from player import *
+from asteroid import *
+from asteroidfield import *
+from shot import *
 
 
 def main():
@@ -13,6 +13,19 @@ def main():
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	dt = 0
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+	shots = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
+	Asteroid.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = (updatable,)
+	Shot.containers = (shots, updatable, drawable)
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	asteroid_field = AsteroidField()
+
 
 # game loop
 	while True:
@@ -20,8 +33,19 @@ def main():
     			if event.type == pygame.QUIT:
         			return
 		screen.fill((0, 0, 0))
+		dt = clock.tick(60) / 1000
+		updatable.update(dt)
+		for asteroid in asteroids:
+			if asteroid.collision(player):
+				print("Game over!")
+				exit()
+			for shot in shots:
+				if asteroid.collision(shot):
+					asteroid.split()
+					shot.kill()
+		for drawn in drawable:
+			drawn.draw(screen)
 		pygame.display.flip()
-
 if __name__ == "__main__":
     main()
 
